9f2d97e92e5b6b97b7aa86cf54396b4560f3aabb
imajoriri <imajo.srroni@gmail.com>
2024-12-01 07:04:24 +0900

    haptics android

--- packages/tokeru_haptics/lib/tokeru_haptics.dart
+++ packages/tokeru_haptics/lib/tokeru_haptics.dart
@@ -1,11 +1,14 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
 import 'tokeru_haptics_platform_interface.dart';
 
 const double heavy = 1;
-const double medium = 0.7;
+const double medium = 0.6;
 const double light = 0.3;
 
-const double rigid = 1;
-const double soft = 0;
+const double rigid = 0.8;
+const double soft = 0.2;
 
 class TokeruHaptics {
   Future<String?> getPlatformVersion() {
@@ -13,6 +16,9 @@ class TokeruHaptics {
   }
 
   static Future<void> rigidHeavy() async {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      HapticFeedback.heavyImpact();
+    }
     return vibrate(
       sharpness: rigid,
       intensity: heavy,
@@ -20,6 +26,9 @@ class TokeruHaptics {
   }
 
   static Future<void> rigidMedium() async {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      HapticFeedback.mediumImpact();
+    }
     return vibrate(
       sharpness: rigid,
       intensity: medium,
@@ -27,6 +36,9 @@ class TokeruHaptics {
   }
 
   static Future<void> rigidLight() async {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      HapticFeedback.lightImpact();
+    }
     return vibrate(
       sharpness: rigid,
       intensity: light,
@@ -34,6 +46,9 @@ class TokeruHaptics {
   }
 
   static Future<void> softHeavy() async {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      HapticFeedback.heavyImpact();
+    }
     return vibrate(
       sharpness: soft,
       intensity: heavy,
@@ -41,6 +56,9 @@ class TokeruHaptics {
   }
 
   static Future<void> softMedium() async {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      HapticFeedback.mediumImpact();
+    }
     return vibrate(
       sharpness: soft,
       intensity: medium,
@@ -48,6 +66,9 @@ class TokeruHaptics {
   }
 
   static Future<void> softLight() async {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      HapticFeedback.lightImpact();
+    }
     return vibrate(
       sharpness: soft,
       intensity: light,
